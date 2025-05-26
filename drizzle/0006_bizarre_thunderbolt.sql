e260f5f387146c731de5eb90b350e30b28696d9d
CK-Daniel <102612621+CK-Daniel@users.noreply.github.com>
2025-05-26 10:59:26 +0000

    1.02

---
+++ drizzle/0006_bizarre_thunderbolt.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `apps` ADD `app_type` text DEFAULT 'react';--> statement-breakpoint
+ALTER TABLE `apps` ADD `mysql_port` integer;--> statement-breakpoint
+ALTER TABLE `apps` ADD `php_port` integer;
